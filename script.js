ce158cd2662dbd7b7e294b13931f75dce033de69
Jugat-Kohli <161903832+Jugat-Kohli@users.noreply.github.com>
2024-08-18 15:49:43 -0700

    div test

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function sleep(ms){
     return new Promise(resolve => setTimeout(resolve,ms));
 }
 function tp(url){
-
+    console.log("Redirecting to", url);
+    sleep(20)
     window.location.href = url;
 }
