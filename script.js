c915189c91e1e1135f4345b0cc5d9b3d80097ab3
Jugat-Kohli <161903832+Jugat-Kohli@users.noreply.github.com>
2024-08-18 17:18:23 -0700

    developed log system and started testing it

--- script.js
+++ script.js
@@ -6,3 +6,35 @@ function tp(url){
     sleep(20)
     window.location.href = url;
 }
+function call(log,type,conditional,){
+    if (type === "log"){
+        console.log(log);
+    }
+    if (type === "error"){
+        console.error(log);
+    }
+    if (type === "info"){
+        console.info(log)
+    }
+    if (type === "warn"){
+        console.warn(log);
+    }
+    if (type === "debug"){
+        console.debug(log);
+    }
+    if (type === "starttimer"){
+        console.time(log);
+    }
+    if (type === "endtimer"){
+        console.time(log);
+    }
+    if (type === "trace"){
+        console.trace(log);
+    }
+    if (type === "assert"){
+        console.assert(conditional,log);
+    }
+    else{
+        console.log("Invalid Type")
+    }
+}
