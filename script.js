11e01c620a9b26719e4cc8243ce222ae6b3cbdad
Jugat-Kohli <161903832+Jugat-Kohli@users.noreply.github.com>
2024-08-27 16:49:23 -0700

    sd

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function sleep(ms){
-    return new Promise(resolve => setTimeout(resolve,ms));
+function sleep(number) {
+    setTimeout(()=>{},number)
 }
+
 function tp(url){
     console.log("Redirecting to", url);
     sleep(100)
