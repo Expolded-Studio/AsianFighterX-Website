8b35688eb9511b22c2beae90328c1f3e30b9bd33
Jugat-Kohli <161903832+Jugat-Kohli@users.noreply.github.com>
2024-09-01 09:43:20 -0700

    +Firebase /Dimension

--- script.js
+++ script.js
@@ -39,3 +39,26 @@ function call(log,type,conditional,){
         console.error("Error Function",type,log,"{Invalid Type}")
     }
 }
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAS3aaO01sy3PWsMhGcy5QOimEFPW4g5zc",
+  authDomain: "asianfighterx-website.firebaseapp.com",
+  databaseURL: "https://asianfighterx-website-default-rtdb.firebaseio.com",
+  projectId: "asianfighterx-website",
+  storageBucket: "asianfighterx-website.appspot.com",
+  messagingSenderId: "791740860737",
+  appId: "1:791740860737:web:1fb095dc77348eb8f7f9c0",
+  measurementId: "G-TXEP8ZM0KR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
