ed1dd3eee778e5ef66ef99a0a4a50ae5d125d3ed
Jugat-Kohli <161903832+Jugat-Kohli@users.noreply.github.com>
2024-09-07 09:56:05 -0700

    debuging

--- script.js
+++ script.js
@@ -5,41 +5,7 @@ function tp(url){
     setTimeout(()=>{},1000)
     window.location.href = url;
 }
-function call(log, type, conditional) {
-    switch (type) {
-        case "log":
-            console.log(log);
-            break;
-        case "error":
-            console.error(log);
-            break;
-        case "info":
-            console.info(log);
-            break;
-        case "warn":
-            console.warn(log);
-            break;
-        case "debug":
-            console.debug(log);
-            break;
-        case "starttimer":
-            console.time(log);
-            break;
-        case "endtimer":
-            console.timeEnd(log);
-            console.timeLog(log);
-            break;
-        case "trace":
-            console.trace(log);
-            break;
-        case "assert":
-            console.assert(conditional, log);
-            break;
-        default:
-            console.error("Error Function: Invalid Type: ", type,"Input:",log);
-            break;
-    }
-}
+
 
 
 // Import the functions you need from the SDKs you need
